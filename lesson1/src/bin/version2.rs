849f1753f6eb791fa06954ee6273a9498c138794
polyviosmusic@gmail.com <polyviosmusic@gmail.com>
2025-11-17 19:28:40 +0100

    version 2 of exercise 1 - stacked variables

---
+++ lesson1/src/bin/version2.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let name = "Master Pierre";
+    let language = "K";
+    let future = "One day";
+    println!("Hello {}! I am using RUST! You use {}!, I will too...{}!", name, language, future);
+
+}
